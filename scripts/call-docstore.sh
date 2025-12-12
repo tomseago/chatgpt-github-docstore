3b2e238d9015264c138d93407fb2c38186d0e8af
Tom Seago <tom@tomseago.com>
2025-12-11 16:53:46 -0800

    Renaming confusing variables

--- scripts/call-docstore.sh
+++ scripts/call-docstore.sh
@@ -12,11 +12,11 @@ if [[ -z "${DOCSTORE_API_TOKEN:-}" ]]; then
 fi
 
 METHOD="${1:-GET}"
-PATH="${2:-/docs}"
+FPATH="${2:-/docs}"
 DATA="${3:-}"
 
 if [[ "$METHOD" == "GET" || "$METHOD" == "DELETE" ]]; then
-  curl -i -X "$METHOD"         -H "Authorization: Bearer ${DOCSTORE_API_TOKEN}"         "${DOCSTORE_WORKER_URL}${PATH}"
+  curl -i -X "$METHOD"         -H "Authorization: Bearer ${DOCSTORE_API_TOKEN}"         "${DOCSTORE_WORKER_URL}${FPATH}"
 else
-  curl -i -X "$METHOD"         -H "Authorization: Bearer ${DOCSTORE_API_TOKEN}"         -H "Content-Type: application/json"         -d "${DATA}"         "${DOCSTORE_WORKER_URL}${PATH}"
+  curl -i -X "$METHOD"         -H "Authorization: Bearer ${DOCSTORE_API_TOKEN}"         -H "Content-Type: application/json"         -d "${DATA}"         "${DOCSTORE_WORKER_URL}${FPATH}"
 fi
